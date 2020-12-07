a235e2835d6332022b1c1bd71127b619178712d7
AnthonyGigerich <43369481+AnthonyGigerich@users.noreply.github.com>
2020-12-07 17:29:10 +0100

    Modified pdfplumber extractor
    Totally changed the way I did it, it's faster and we can store the metadata now
    
    /*Todo*/
    return a json file instead of plain text

--- src/data/extract_pdfplumber.py
+++ src/data/extract_pdfplumber.py
@@ -1,23 +1,10 @@
-"""Create a dataset from PDF files."""
-
 import pdfplumber
 import io
 import re
+import os
+from pathlib import Path
 import numpy as np
 import timeit
-
-
-# Parameter: pdf: a pdf
-#           font_size: int
-#
-# Output: pdf with any character with a font size > font_size removed
-def remove_big_font_char(pdf, font_size):
-    for page in pdf.pages:
-        for char in page.chars:
-            if char["size"] > font_size:
-                char["text"] = ""
-    return pdf
-
 
 # Parameter: table: 2D array
 #
@@ -33,141 +20,208 @@ def table_is_empty(table):
 
 # Parameter: page: page from a pdf
 #
-# Output: True if the page is empty
-#        False if the page is not
-def page_is_empty(page):
-
-    setting = {
-        "vertical_strategy": "text",
-        "horizontal_strategy": "text",
-    }
-
-    table = page.extract_table(setting)
-    if table is None:
-        print("Remove page n°", page.page_number)
-        return True
-    else:
-        return False
-
-
-# Parameter: page: page from a pdf
-#
 # Output: True if the page has column
 #        False if the page does not have column
 def page_has_column(page):
 
-    setting = {
+    height = page.height
+    width = page.width
+
+    setting_left = {
         "vertical_strategy": "explicit",
         "horizontal_strategy": "explicit",
-        "explicit_vertical_lines": [height, width / 2 - 15, height, width / 2 + 15],
+        "explicit_vertical_lines": [height, width / 6, height, width / 5],
         "explicit_horizontal_lines": [100, width, height - 50, width],
     }
 
-    table = page.extract_table(setting)
-    if table_is_empty(table) and not page_is_empty(page):
+    setting_middle = {
+        "vertical_strategy": "explicit",
+        "horizontal_strategy": "explicit",
+        "explicit_vertical_lines": [height, width / 2 - 8, height, width / 2 + 8],
+        "explicit_horizontal_lines": [125, width, height - 50, width],
+    }
+
+    table_middle = page.extract_table(setting_middle)
+    table_left = page.extract_table(setting_left)
+    if table_is_empty(table_middle) and not table_is_empty(table_left):
         return True
     else:
         print("Remove page n°", page.page_number)
         return False
 
 
-# Parameter: pdf: a pdf
-#           setting: settings for the extract table function (see doc)
-# Output: a texte file with arrêté splitted
-def split_arrete(pdf, setting):
-    print("cleaning...")
-    clean_start = timeit.default_timer()
-    pdf = remove_big_font_char(pdf, 10)
-    clean_stop = timeit.default_timer()
-    print("pdf cleaned in ", clean_stop - clean_start)
+def is_bold(fontname):
+    r = re.compile(".*bold.*")
+    if re.match(r, fontname.lower()):
+        return True
+    else:
+        return False
 
-    height = pdf.pages[0].height
-    width = pdf.pages[0].width
 
-    regex = re.compile("^[0-9]*/[0-9]* *– *")
-    regex1 = re.compile("^N° *[0-9]*_*[0-9]*_VDM *[A-Z]")
-    regex2 = re.compile("^article [0-9]*  |^vu ")
+def pdf_cleaner(pdf):
 
-    result = ""
-
-    previous_line_empty = False
+    clean_pdf = []
     for page in pdf.pages:
         crop = page.crop((0, 50, page.width, page.height))
-        if page_has_column(crop):  # Si la page est sous un format valide
-            table = crop.extract_table(
-                setting
-            )  # Sépare le texte selon le setting en paramètre
-            array = np.array(table)
-            for column in range(2):  # Pour chacune des deux colonnes
-                for line in array[:, column]:  # Pour chaque ligne
-                    if len(line) > 0:  # Si ce n'est pas une ligne vide
-
-                        temp = np.array2string(
-                            line
-                        )  # Renvoie un string de la forme <'string'>
-                        string = temp[
-                            1 : len(temp) - 1
-                        ]  # Retire les <'> pour obtenir <string>
-
-                        if (
-                            (previous_line_empty and string.startswith("N° "))
-                            or re.match(regex, string)
-                            or re.match(regex1, string)
-                        ):  # Detecte les arretés
-                            result += "\n\n\n\n"
+        if page_has_column(crop):
+            current_page = []
+            left_column = []
+            right_column = []
+            left_line = []
+            right_line = []
+            old_y = crop.chars[0]["y0"]
+            for char in crop.chars:
+                if char["x0"] < crop.width / 2:
+                    if char["size"] <= 10:
+                        if char["y0"] == old_y:
+                            left_line.append(char)
                         else:
-                            if (
-                                previous_line_empty
-                                and string.lower().startswith("article ")
-                            ) or re.match(
-                                regex2, string.lower()
-                            ):  # Detecte les différentes parties de l'arreté
-                                result += "\n"
+                            old_y = char["y0"]
+                            left_column.append(left_line)
+                            left_line = []
+                            left_line.append(char)
+                else:
+                    if char["size"] <= 10:
+                        if char["y0"] == old_y:
+                            right_line.append(char)
+                        else:
+                            old_y = char["y0"]
+                            right_column.append(right_line)
+                            right_line = []
+                            right_line.append(char)
+            current_page.append(left_column)
+            current_page.append(right_column)
+            clean_pdf.append(current_page)
+    return clean_pdf
+    print("done")
 
-                        result += string + " "
-                        previous_line_empty = False
+
+def get_arrete_from_clean_pdf(clean_pdf):
+
+    regex_arrete = re.compile("^N° *[0-9]+_+[0-9]+_VDM|^[0-9]+/[0-9]+ *– *")
+    regex_article = re.compile("^article *[0-9]* +|^Article *[0-9]* +|^ARTICLE *[0-9]* +")
+    regex_intro = re.compile("^vu +|^considérant")
+
+    arretes = []
+    current_arrete = []
+    current_title = []
+    current_intro_list = []
+    current_intro = []
+    current_article_list = []
+    current_article = []
+
+    title_flag = True
+    intro_flag = False
+    article_flag = False
+
+    for page in clean_pdf:
+        for column in page:
+            for line in column:
+                string = ""
+                for c in line:
+                    string += c["text"]
+                if title_flag:
+                    if re.match(regex_intro, string.lower()):
+                        title_flag = False
+                        intro_flag = True
+                        current_arrete.append(current_title)
+                        current_title = []
+                        current_intro.append(line)
                     else:
-                        previous_line_empty = True
+                        current_title.append(line)
+                else:
+                    if intro_flag:
+                        if re.match(regex_intro, string.lower()):
+                            current_intro_list.append(current_intro)
+                            current_intro = []
+                            current_intro.append(line)
+                        else:
+                            if re.match(regex_article, string.lower()) and is_bold(
+                                line[0]["fontname"]
+                            ):
+                                intro_flag = False
+                                article_flag = True
+                                current_intro_list.append(current_intro)
+                                current_intro = []
+                                current_arrete.append(current_intro_list)
+                                current_intro_list = []
+                                current_article.append(line)
+                            else:
+                                current_intro.append(line)
+                    else:
+                        if article_flag:
+                            if re.match(regex_article, string.lower()) and is_bold(
+                                line[0]["fontname"]
+                            ):
+                                current_article_list.append(current_article)
+                                current_article = []
+                                current_article.append(line)
+                            else:
+                                if re.match(regex_arrete, string) and is_bold(
+                                    line[0]["fontname"]
+                                ):
+                                    article_flag = False
+                                    title_flag = True
+                                    current_article_list.append(current_article)
+                                    current_article = []
+                                    current_arrete.append(current_article_list)
+                                    current_article_list = []
+                                    arretes.append(current_arrete)
+                                    current_arrete = []
+                                    current_title.append(line)
+                                else:
+                                    current_article.append(line)
 
-    output = io.open(
-        r"data\text\out.txt", "w", encoding="utf-8"
-    )  # Ecrit dans le fichier output
-    output.write(result)
-    output.close()
+    current_article_list.append(current_article)
+    current_article = []
+    current_arrete.append(current_article_list)
+    current_article_list = []
+    arretes.append(current_arrete)
+    current_arrete = []
+    return arretes
 
+
+start = timeit.default_timer()
 
 pdf = pdfplumber.open(
     r"C:\Users\Anthony\Downloads\demi-ou-moitie-main\data\raw\raa_610.pdf"
 )
 
-height = pdf.pages[0].height
-width = pdf.pages[0].width
+clean_pdf = pdf_cleaner(pdf)
+arretes = get_arrete_from_clean_pdf(clean_pdf)
 
+result = ""
 
-# Génération des lignes pour séparer le texte horizontalement
-ys = []
-for y in range(0, int(height), 7):
-    ys.append(int(width))
-    ys.append(y)
+for arrete in arretes:
+    titles = arrete[0]
+    intros = arrete[1]
+    articles = arrete[2]
+    for line in titles:
+        for char in line:
+            result += char["text"]
+        result += " "
+    result += "\n"
+    for intro in intros:
+        for line in intro:
+            for char in line:
+                result += char["text"]
+            result += " "
+        result += "\n"
+    for article in articles:
+        for line in article:
+            for char in line:
+                result += char["text"]
+            result += " "
+        result += "\n"
+    result += "\n"
+    result += "\n"
 
-setting = {
-    "vertical_strategy": "explicit",  # Explicit: je définis les lignes de séparations
-    "horizontal_strategy": "explicit",  # Text: Les lignes de textes définissent les séparations
-    "explicit_vertical_lines": [
-        height,
-        30,
-        height,
-        width / 2,
-        height,
-        width - 30,
-    ],  # Ligne explicit: A gauche, au mileu, a droite pour séparer les colonnes
-    "explicit_horizontal_lines": ys,
-}
-
-
-start = timeit.default_timer()
-
-split_arrete(pdf, setting)
+output = io.open(
+    r"data/out/" + r"raa_610" + r".txt", "w", encoding="utf-8"
+)  # Ecrit dans le fichier output
+output.write(result)
+output.close()
 
 stop = timeit.default_timer()
+
 print("Run Time: ", stop - start)
