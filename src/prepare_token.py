e6b06a9ebc540d899111f3209eb1ecb8b527cf05
AnthonyGigerich <43369481+AnthonyGigerich@users.noreply.github.com>
2021-01-12 14:56:01 +0100

    ajout du pont entre clustering et allennlp
    Prend en entrée une liste d'arrété concernant des terrasses sous format json et génère des fichiers .txt sous le format d'entrée d'AllenNLP

---
+++ src/prepare_token.py
@@ -0,0 +1,144 @@
+import json
+
+#entrée: un mot avec une apostrophe | ie. "l'article"
+#sortie: deux mots séparés par l'apostrope | ie. "l'" et "article"
+def split_apostrophe(word):
+    for i, char in enumerate(word):
+        if char == "'":
+            return word[:i+1], word[i+1:]
+    print("oops")
+    return 1
+
+#Entrée: arrete sous format json
+#Sortie: arrete sous format .txt séparé en entité nommé pour AllenNLP
+def prepare_tokens(arrete):
+
+    f = open(
+        r"data/test/" + arrete["title"][0]["text"][0:17] + ".txt",
+        "w",
+        encoding="utf-8",
+    )
+
+    flag_line = False
+
+    result = ""
+    for line in arrete["title"]:
+        words = line["text"].split(" ")
+        for word in words:
+
+            todo = []
+            if "'" in word:
+                word1, word2 = split_apostrophe(word)
+                todo.append(word1)
+                todo.append(word2)
+            else:
+                todo.append(word)
+            for word in todo:
+                if len(word) > 0:
+                    if (
+                        word[len(word) - 1] == ","
+                        or (word[len(word) - 1] == "." and word[len(word) - 2] != "L")
+                        or word[len(word) - 1] == "?"
+                        or word[len(word) - 1] == "!"
+                    ):
+                        if word[: len(word) - 1] != "":
+                            result += word[: len(word) - 1] + " " + "O" + "\n"
+                        if word[len(word) - 1 :] != "":
+                            result += word[len(word) - 1 :] + " " + "O" + "\n"
+                        flag_line = False
+                        if word[len(word) - 1] == ".":
+                            result += "\n"
+                            flag_line = True
+                    else:
+                        flag_line = False
+                        if word != "":
+                                result += word + " " + "O" + "\n"
+
+    if not flag_line:
+        result += "\n"
+
+    flag_line = False
+    for intro in arrete["intros"]:
+        for line in intro:
+            words = line["text"].split(" ")
+            for word in words:
+                todo = []
+                if "'" in word:
+                    word1, word2 = split_apostrophe(word)
+                    todo.append(word1)
+                    todo.append(word2)
+                else:
+                    todo.append(word)
+                for word in todo:
+                    if len(word) > 0:
+                        if (
+                            word[len(word) - 1] == ","
+                            or (
+                                word[len(word) - 1] == "."
+                                and word[len(word) - 2] != "L"
+                            )
+                            or word[len(word) - 1] == "?"
+                            or word[len(word) - 1] == "!"
+                        ):
+                            if word[: len(word) - 1] != "":
+                                result += word[: len(word) - 1] + " " + "O" + "\n"
+                            if word[len(word) - 1 :] != "":
+                                result += word[len(word) - 1 :] + " " + "O" + "\n"
+                            flag_line = False
+                            if word[len(word) - 1] == ".":
+                                result += "\n"
+                                flag_line = True
+                        else:
+                            if word != "":
+                                result += word + " " + "O" + "\n"
+                            flag_line = False
+        if not flag_line:
+            result += "\n"
+
+    flag_line = False
+    for article in arrete["articles"]:
+        for line in article:
+            words = line["text"].split(" ")
+            for word in words:
+                todo = []
+                if "'" in word:
+                    word1, word2 = split_apostrophe(word)
+                    todo.append(word1)
+                    todo.append(word2)
+                else:
+                    todo.append(word)
+                for word in todo:
+                    if len(word) > 0:
+                        if (
+                            word[len(word) - 1] == ","
+                            or (
+                                word[len(word) - 1] == "."
+                                and word[len(word) - 2] != "L"
+                            )
+                            or word[len(word) - 1] == "?"
+                            or word[len(word) - 1] == "!"
+                        ):
+                            if word[: len(word) - 1] != "":
+                                result += word[: len(word) - 1] + " " + "O" + "\n"
+                            if word[len(word) - 1 :] != "":
+                                result += word[len(word) - 1 :] + " " + "O" + "\n"
+                            flag_line = False
+                            if word[len(word) - 1] == ".":
+                                result += "\n"
+                                flag_line = True
+                        else:
+                            if word != "":
+                                result += word + " " + "O" + "\n"
+                            flag_line = False
+        if not flag_line:
+            result += "\n"
+
+    f.write(result)
+    f.close()
+
+
+with open(r"data/out/terrasses.json") as json_file:
+    data = json.load(json_file)
+
+for arrete in data["arretes"]:
+    prepare_tokens(arrete)
