589728b0c212623718b547fb968fbdab260f2d30
AnthonyGigerich <43369481+AnthonyGigerich@users.noreply.github.com>
2021-01-13 23:04:52 +0100

    Update folder.py
    petite modification pour le nouveau format / vérifie que nos coordonnées sont bien dans marseille

--- src/interactive_map/folder.py
+++ src/interactive_map/folder.py
@@ -1,65 +1,87 @@
 import folium
 import json
+import math
 
 
-#Input: json file
-#Output: Interactive map as HTML
+# Vérifie que les coordonnées sont bien dans marseille
+def store_in_marseille(x, y):
+
+    cx = 43.2961743
+    cy = 5.3699525
+
+    print(math.sqrt((x - cx) ** 2 + (y - cy) ** 2))
+
+    if math.sqrt((x - cx) ** 2 + (y - cy) ** 2) < 0.5:
+        return True
+    else:
+        return False
+
+
+# Input: json file
+# Output: Interactive map as HTML
 def get_interactive_map(json_path):
-    with open(
-        json_path
-    ) as json_file:
+    with open(json_path) as json_file:
         data = json.load(json_file)
 
-    m = folium.Map(location=[43.2961743, 5.3699525], tiles="Stamen Terrain", zoom_start=13)
+    m = folium.Map(
+        location=[43.2961743, 5.3699525], tiles="Stamen Terrain", zoom_start=13
+    )
 
-    for store in data:
+    if "terrasses" in data.keys():
+        for store in data["terrasses"]:
 
-        if store["type"] == "terrasse":
+            if store_in_marseille(store["locs"][0], store["locs"][1]):
 
-            marker_html = (
-                "<b>"
-                + store["societe"]
-                + "</b><b>   |   TERRASSE</b><br><b>Adresse:</b> "
-                + store["adresses"][0]
-            )
+                marker_html = "<b>"
 
-            if store["adresses"][1] != "":
-                marker_html += "<br><b>Adresse 2:</b> " + str(store["adresses"][1])
+                if store["societe"] != "NA":
+                    marker_html += store["societe"] + "</b><b>   |   TERRASSE</b><br>"
 
-            if store["facade"] != 0:
-                marker_html += "<br><b>Taille de façade:</b> " + str(store["facade"]) + " m"
+                if store["adresses"] != "NA":
+                    marker_html += "<b>Adresse:</b> " + store["adresses"]
 
-            if store["largeur"] != 0:
-                marker_html += "<br><b>Largeur:</b> " + str(store["largeur"]) + " m"
+                if store["facade"] != "NA":
+                    marker_html += (
+                        "<br><b>Taille de façade:</b> " + str(store["facade"]) + " m"
+                    )
 
-            if store["superficie"] != 0:
-                marker_html += "<br><b>Superficie:</b> " + str(store["superficie"]) + " m"
+                if store["largeur"] != "NA":
+                    marker_html += "<br><b>Largeur:</b> " + str(store["largeur"]) + " m"
 
-            if store["date"] != "":
-                marker_html += "<br><b>Date d'ouverture:</b> " + store["date"]
+                if store["superficie"] != "NA":
+                    marker_html += (
+                        "<br><b>Superficie:</b> " + str(store["superficie"]) + " m²"
+                    )
 
-            folium.Marker(
-                store["locs"],
-                popup=folium.Popup(
-                    marker_html,
-                    max_width=1000,
-                ),
-                tooltip="<b>" + store["societe"] + "</b>",
-                icon=folium.Icon(color="blue", icon="university", prefix="fa"),
-            ).add_to(m)
+                if store["date"] != "NA":
+                    marker_html += "<br><b>Date de l'arrété:</b> " + store["date"]
 
-        if store["type"] == "pizza":
+                folium.Marker(
+                    store["locs"],
+                    popup=folium.Popup(
+                        marker_html,
+                        max_width=1000,
+                    ),
+                    tooltip="<b>" + store["societe"] + "</b>",
+                    icon=folium.Icon(color="blue", icon="university", prefix="fa"),
+                ).add_to(m)
+
+    if "pizza" in data.keys():
+        for store in data["pizza"]:
             for i, loc in enumerate(store["locs"]):
 
-                marker_html = (
-                    "<b>"
-                    + store["societe"]
-                    + "</b><b>   |   CAMION PIZZA</b><br><b>Adresse:</b> "
-                    + store["adresses"][i]
-                )
+                marker_html = "<b>"
+
+                if store["societe"] != "NA":
+                    marker_html += (
+                        store["societe"] + "</b><b>   |   Camion Pizza</b><br>"
+                    )
+
+                if store["adresses"] != "NA":
+                    marker_html += "<b>Adresse:</b> " + store["adresses"][i]
 
                 if store["date"] != "":
-                    marker_html += "<br><b>Date d'ouverture:</b> " + store["date"]
+                    marker_html += "<br><b>Date de l'arrété:</b> " + store["date"]
 
                 folium.Marker(
                     loc,
@@ -72,6 +94,9 @@ def get_interactive_map(json_path):
                 ).add_to(m)
     return m
 
-#if __name__ == "__main__":
-    #map = get_interactive_map(r'C:\Users\Anthony\Downloads\demi-ou-moitie-main\src\interactivemap\json_test.json')
-    #map.save("index.html")
+
+if __name__ == "__main__":
+    map = get_interactive_map(
+        r"C:\Users\Anthony\Downloads\demi-ou-moitie-main\src\interactivemap\test3.json"
+    )
+    map.save("index.html")
