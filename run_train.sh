a8e10b51520719734cb44876dd0ba49cfdfc97d6
Mathieu Morey <mathieu.morey@gmail.com>
2021-01-21 21:02:26 +0100

    FIX revert run_train

--- run_train.sh
+++ run_train.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 scriptdir=`dirname "$BASH_SOURCE"`
 
-# FIXME dirty copy of run.sh, to force training
-if [[ -d src/NLP/data/toPredict ]]
-then
-    rm src/NLP/data/toPredict/*.txt
-else
-    mkdir src/NLP/data/toPredict
-fi
+rm src/NLP/data/toPredict/*.txt
 mkdir src/NLP/data/temp_for_extraction
 unzip src/NLP/data/toPredict.zip -d src/NLP/data/temp_for_extraction/
 cp src/NLP/data/temp_for_extraction/toPredict/*.txt src/NLP/data/toPredict/
